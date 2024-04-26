e4fca98eed1d6122e33841aad94460ca6761a664
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-04-26 17:24:22 +0300

    Create teconnectivity_scraper.py

---
+++ sites/teconnectivity_scraper.py
@@ -0,0 +1,30 @@
+from src.scrapers import Scraper
+
+
+class TeConnectivity(Scraper):
+    def __init__(self, company_name, url, logo_url):
+        super().__init__(company_name, url, logo_url)
+
+    def get_jobs(self):
+
+        response = self.get_soup()
+        jobs = response.find_all('tr', class_='data-row')
+
+        for job in jobs:
+            link = 'https://careers.te.com' + job.find('a', class_='jobTitle-link')['href']
+            title = job.find('a', class_='jobTitle-link').text
+            city = str(job.find('span', class_='jobLocation').text.split(',')[0].strip()).capitalize()
+            job_type = 'remote' if 'remote' in title.lower() else 'on-site'
+
+            self.get_jobs_dict(title, link, city, job_type)
+
+        return self.jobs_list
+
+
+teconnectivity = TeConnectivity(
+    company_name='TeConnectivity',
+    url='https://careers.te.com/search/?createNewAlert=false&q=&locationsearch=Romania&optionsFacetsDD_customfield3=&optionsFacetsDD_department=',
+    logo_url='https://rmkcdn.successfactors.com/e2907dff/905ce309-95ed-4020-875d-3.jpg'
+)
+teconnectivity.get_jobs()
+teconnectivity.push_peviitor()
