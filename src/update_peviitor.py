de479b627525c20535e9519c0dba0499cd45ccc6
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-05-31 21:10:18 +0300

    Update update_peviitor.py

--- src/update_peviitor.py
+++ src/update_peviitor.py
@@ -19,7 +19,6 @@ class UpdatePeViitor:
             'Content-Type': 'application/json',
         }
 
-
     def get_token(self):
         #token_endpoint = 'https://api.peviitor.ro/v5/get_token/'
         token_endpoint = 'https://api.laurentiumarian.ro/get_token'
@@ -31,20 +30,18 @@ class UpdatePeViitor:
         return token.json()['access']
 
 
-    def update_jobs(self, company_name: str, data_jobs: list):
-
+    def update_jobs(self, company, data_jobs: list):
 
         # time sleep for SOLR indexing
         time.sleep(0.2)
 
-        post_request_to_server = requests.post(self.post_url, headers=self.post_header,
-                                               data=json.dumps(data_jobs))
-
+        requests.post(self.post_url, headers=self.post_header, data=json.dumps(data_jobs))
+        print(json.dumps(data_jobs, indent=4))
 
     def update_logo(self, id_company: str, logo_link: str):
 
         data = json.dumps([{"id": id_company, "logo": logo_link}])
-        response = requests.post(self.logo_url, headers=self.logo_header, data=data)
+        requests.post(self.logo_url, headers=self.logo_header, data=data)
 
 
 
