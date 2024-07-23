cd27946af6ec8fd5bddb53bd058bcdeebb6c0e3e
Alexandru93s <barbulescualexandru93@gmail.com>
2024-07-23 22:05:32 +0300

    Create nexterp.py

---
+++ sites/nexterp.py
@@ -0,0 +1,27 @@
+from src.scrapers import Scraper
+
+
+class Nexterp(Scraper):
+    def __init__(self, company_name, url, logo_url):
+        super().__init__(company_name, url, logo_url)
+
+    def get_jobs(self):
+        jobs = self.get_soup().find_all('a', class_='text-decoration-none')
+        for job in jobs:
+            link = "https://www.nexterp.ro" + job.get('href')
+            title = job.find('h3').text.strip()
+            city = self.get_link_soup(link).find('div', class_='d-flex align-items-baseline').text.split(',')[0].strip()
+            self.get_jobs_dict(title,link,city)
+    
+        return self.jobs_list
+
+
+
+nexterp = Nexterp(
+    company_name="NextERP",
+    url="https://www.nexterp.ro/ro/jobs",
+    logo_url=''
+
+)
+nexterp.get_jobs()
+nexterp.push_peviitor()
