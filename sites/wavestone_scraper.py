7e15446af70484906aeef34c6c631010c130ccd1
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-07-23 21:16:29 +0300

    Create wavestone_scraper.py

---
+++ sites/wavestone_scraper.py
@@ -0,0 +1,74 @@
+from src.scrapers import Scraper
+
+
+class WaveStone(Scraper):
+    def __init__(self, company_name, url, logo_url):
+        super().__init__(company_name, url, logo_url)
+
+    def get_jobs(self):
+
+        payload = {
+            "userId": "2c2a4ca7-b744-48bb-a201-898565e05b60",
+            "projectId": "6b078a44-931a-40b3-be4f-6e76f550f6df",
+            "pageId": "ebeb3826-c6dd-4ac7-befa-d0b0ffde8515",
+            "isStandalone": False,
+            "locale": "en",
+            "isActiveCustomJobPages": True,
+            "isUseLayoutsOfSubsidiaries": False,
+            "filterStatus": {
+                "careerLevel": True,
+                "category": False,
+                "partnership": False,
+                "location": True,
+                "company": False
+            },
+            "pageNumber": 1,
+            "numberOfJobsOnPage": 20,
+            "listState": {
+                "search": "",
+                "location": {},
+                "filters": {"location": ["Romania"]},
+                "filterPredictions": {}
+            },
+            "jobListTitleNew": "New",
+            "isGetFilters": True,
+            "isForCurrentLocale": True
+        }
+        headers = {
+            "authority": "pcw-api.softgarden.de",
+            "method": "POST",
+            "path": "/widget-api/job-list/jobAds/",
+            "scheme": "https",
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "ro-RO,ro;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Content-Type": "application/json",
+            "Origin": "https://romania-career.wavestone.com",
+            "Priority": "u=1, i",
+            "Referer": "https://romania-career.wavestone.com/",
+            "Sec-Ch-Ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+            "Sec-Ch-Ua-Mobile": "?1",
+            "Sec-Ch-Ua-Platform": "\"Android\"",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "cross-site",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36"
+
+        }
+        jobs = self.post_json(json=payload, headers=headers)['data']['jobs']
+
+        for job in jobs:
+            title = job['title']
+            link = 'https://romania-career.wavestone.com' + job['link']
+            self.get_jobs_dict(title, link, 'Cluj-Napoca', 'hybrid')
+
+        return self.jobs_list
+
+
+wavestone = WaveStone(
+    company_name='wavestone',
+    url='https://pcw-api.softgarden.de/widget-api/job-list/jobAds/',
+    logo_url=''
+)
+wavestone.get_jobs()
+wavestone.push_peviitor()
