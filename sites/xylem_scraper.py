53a4e3ed4ee63562b789621c692f2a667a9d3dbf
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-09-05 15:28:35 +0300

    Update xylem_scraper.py

--- sites/xylem_scraper.py
+++ sites/xylem_scraper.py
@@ -1,3 +1,5 @@
+from numpy.core.defchararray import title
+
 from src.scrapers import Scraper
 
 
@@ -7,22 +9,30 @@ class Xylem(Scraper):
 
     def get_jobs(self):
 
-        response = self.get_soup()
-        jobs = response.find_all('tr')
+        payload = {
+            "appliedFacets": {"locationCountry": ["f2e609fe92974a55a05fc1cdc2852122"]},
+            "limit": 20,
+            "offset": 0,
+            "searchText": ""
+        }
+        headers = self.get_cookies("wd-browser-id", "PLAY_SESSION", "wday_vps_cookie", "__cf_bm", "__cflb", "_cfuvid")
+        jobs = self.post_json(headers=headers, json=payload)['jobPostings']
 
         for job in jobs:
-            title = job.find('a').text
-            link = 'https://jobs.jobvite.com/' + job.find('a')['href']
-            city = job.find('td', class_='jv-job-list-location').text.split(',')[0].strip()
-
-            if validated_city := self.get_validated_city(city):
-                self.get_jobs_dict(title, link, validated_city)
+            title = job['title']
+            link = "https://xylem.wd5.myworkdayjobs.com/en-US/xylem-careers" + job['externalPath']
+            link_job_type = "https://xylem.wd5.myworkdayjobs.com/wday/cxs/xylem/xylem-careers"
+            job_type = "hybrid" if self.get_json_link(link_job_type + job['externalPath'])['jobPostingInfo'].get('remoteType') \
+                else "on-site"
+            self.get_jobs_dict(title, link, "Bucuresti", job_type)
 
         return self.jobs_list
 
+
+
 xylem = Xylem(
     company_name='xylem',
-    url='https://jobs.jobvite.com/xylem/search?r=&l=Bucharest,%20Romania&c=&q=',
+    url='https://xylem.wd5.myworkdayjobs.com/wday/cxs/xylem/xylem-careers/jobs',
     logo_url='https://www.xylem.com/siteassets/brand/_logos/color-lockups-transparent/xylem_tag_7704c.svg'
 )
 
