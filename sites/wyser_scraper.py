ff7bedad3078d4f4e884fa520b57c7ec58958203
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-10-10 17:39:10 +0300

    Update wyser_scraper.py

--- sites/wyser_scraper.py
+++ sites/wyser_scraper.py
@@ -2,8 +2,6 @@ from src.scrapers import Scraper
 
 
 class Wyser(Scraper):
-    def __init__(self, company_name, url, logo_url):
-        super().__init__(company_name, url, logo_url)
 
     def get_jobs(self):
 
