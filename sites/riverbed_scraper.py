ab6addb8ae181da8897f6b8c09b0b98ab97964d0
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-10-10 17:26:53 +0300

    Update riverbed_scraper.py

--- sites/riverbed_scraper.py
+++ sites/riverbed_scraper.py
@@ -2,8 +2,6 @@ from src.scrapers import Scraper
 
 
 class Riverbed(Scraper):
-    def __init__(self, company_name, url, logo_url):
-        super().__init__(company_name, url, logo_url)
 
     def get_jobs(self):
         querystring = {"ss": "1", "searchLocation": "13526", "in_iframe": "1"}
