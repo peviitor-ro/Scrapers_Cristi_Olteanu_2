a1140534fe92c0f07b6c52b8999910a3f86a8143
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-10-10 17:17:13 +0300

    Update exesoftware.py

--- sites/exesoftware.py
+++ sites/exesoftware.py
@@ -4,9 +4,7 @@ from src.scrapers import Scraper
 
 
 class ExeSoftware(Scraper):
-    def __init__(self, company_name, url, logo_url):
-        super().__init__(company_name, url, logo_url)
-
+    
     def get_jobs(self):
         response = self.get_soup()
         jobs = response.find_all('div', class_='uncont')
