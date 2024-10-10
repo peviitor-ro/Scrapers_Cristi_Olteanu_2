382c55efe9748742af0de075d4080aa6a81d10ef
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-10-10 17:27:40 +0300

    Update sennder_scraper.py

--- sites/sennder_scraper.py
+++ sites/sennder_scraper.py
@@ -3,8 +3,6 @@ import re
 
 
 class Sennder(Scraper):
-    def __init__(self, company_name, url, logo_url):
-        super().__init__(company_name, url, logo_url)
 
     def get_jobs(self):
         response = self.get_soup()
