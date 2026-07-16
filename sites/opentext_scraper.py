784421e3dba7391502be657abd8cf3b832377851
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-07-16 23:26:01 +0300

    feat: refactor job extraction logic and improve pagination handling

--- sites/opentext_scraper.py
+++ sites/opentext_scraper.py
@@ -1,3 +1,5 @@
+import json
+import re
 from src.scrapers import Scraper
 from src.validate_city import validate_city
 
@@ -5,39 +7,58 @@ from src.validate_city import validate_city
 class OpenText(Scraper):
 
     def get_jobs(self):
-        page = 0
-        flag = True
+        from_page = 0
+        page_size = 10
 
-        while flag:
-            jobs = self.get_link_soup(self.url + str(page)).find_all('tr', class_='data-row')
+        while True:
+            url = self.url if from_page == 0 else f'{self.url}&from={from_page}'
+            soup = self.get_soup(url)
 
-            if len(jobs) > 0:
+            jobs_data = self._extract_jobs_from_page(soup)
+            if jobs_data is None:
+                break
 
-                for job in jobs:
-                    link = 'https://careers.opentext.com' + job.find('a', class_='jobTitle-link')['href']
-                    title = job.find('a', class_='jobTitle-link').text
-                    city = job.find('span', class_='jobLocation').text.split(', ')[-2].strip()
-                    country = job.find('span', class_='jobLocation').text.split(', ')[-1].split()[0]
+            jobs = jobs_data.get('data', {}).get('jobs', [])
+            total_hits = jobs_data.get('totalHits', 0)
 
-                    if country != 'RO':
-                        cities = self.get_link_soup(link).find_all('span', class_='jobGeoLocation')
-                        for item in cities:
-                            if 'RO' in item.text:
-                                city = item.text.split(',')[0].strip('\n')
+            for job in jobs:
+                if job.get('country') != 'ROU':
+                    continue
 
-                    self.get_jobs_dict(title, link, validate_city(city))
+                title = job['title']
+                job_id = job['jobId']
+                slug = re.sub(r'[^a-z0-9]+', '-', title.lower()).strip('-')
+                link = f'https://careers.opentext.com/us/en/job/{job_id}/{slug}'
+                city = validate_city(job.get('city', ''))
 
-            else:
-                flag = False
-            page += 25
+                self.get_jobs_dict(title, link, city)
+
+            from_page += page_size
+            if from_page >= total_hits:
+                break
 
         return self.jobs_list
+
+    @staticmethod
+    def _extract_jobs_from_page(soup):
+        for script in soup.find_all('script'):
+            text = script.string or ''
+            if 'eagerLoadRefineSearch' not in text:
+                continue
+            idx = text.find('"eagerLoadRefineSearch":')
+            if idx < 0:
+                continue
+            start = text.find('{', idx)
+            decoder = json.JSONDecoder()
+            data, _ = decoder.raw_decode(text, start)
+            return data
+        return None
+
 
 open_text = OpenText(
     company_name='opentext',
     logo_url='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/OpenText_logo.svg/447px-OpenText_logo.svg.png',
-    url='https://careers.opentext.com/search/?q=&locationsearch=RO&startrow='
-
+    url='https://careers.opentext.com/us/en/search-results?p=ChIJw3aJlSb_sUARlLEEqJJP74Q&location=Romania'
 )
 open_text.get_jobs()
 open_text.push_peviitor()
