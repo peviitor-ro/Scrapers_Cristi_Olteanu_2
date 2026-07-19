14272a0c983a6d06991707052e3ff35330170f91
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-07-19 23:19:41 +0300

    feat: refactor job extraction logic to improve handling of pagination and job details

--- sites/rws_scraper.py
+++ sites/rws_scraper.py
@@ -4,27 +4,59 @@ from src.scrapers import Scraper
 class Rws(Scraper):
 
     def get_jobs(self):
-        querystring = {"ss": "1", "in_iframe": "1", "searchLocation": "13526--"}
-        response = self.get_soup(params=querystring)
-        jobs = response.find_all('div', class_='row')
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.9,ro;q=0.8',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1',
+        }
 
-        for job in jobs:
-            text = job.find('a', class_='iCIMS_Anchor')
+        while True:
+            soup = self.get_soup(params={
+                'ss': '1',
+                'in_iframe': '1',
+                'searchLocation': '13526--Remote',
+            })
 
-            if text is not None:
-                part_title = text['title'].split("-")
-                title = str(''.join(part_title[1:])).strip()
-                link = text['href']
-                job_type = 'remote' if 'remote' in self.get_link_soup(link).find('div', class_='col-xs-6 header left').text.strip().lower() else 'on-site'
+            rows = soup.find_all('div', class_='row')
+            found = False
 
-                self.get_jobs_dict(title, link, 'Cluj-Napoca', job_type)
+            for row in rows:
+                anchor = row.find('a', class_='iCIMS_Anchor')
+                if not anchor:
+                    continue
+
+                full_title = anchor.get('title', '').strip()
+                parts = full_title.split(' - ', 1)
+                title = parts[1].strip() if len(parts) > 1 else full_title
+
+                link = anchor.get('href', '')
+                self.get_jobs_dict(title, link, 'Cluj-Napoca')
+                found = True
+
+            if not found:
+                break
+
+            paginator = soup.find('select', id='iCIMS_Paginator')
+            if not paginator:
+                break
+
+            next_page = paginator.find('option', string=lambda s: s and s.strip().isdigit() and int(s.strip()) > 1)
+            if not next_page:
+                break
+
+            self.url = next_page.get('value', self.url)
 
         return self.jobs_list
 
 
 rws = Rws(
     company_name='rws',
-    url='https://globalcareers-rws.icims.com/jobs/search#iCIMS_Header',
+    url='https://globalcareers-rws.icims.com/jobs/search',
     logo_url='https://c-13850-20230914-www-rws-com.i.icims.com/media/images/Artboard-1_tcm228-187294.svg?v=NjM4MjcwODY1NTA3MTQyMTkw',
 )
 rws.get_jobs()
