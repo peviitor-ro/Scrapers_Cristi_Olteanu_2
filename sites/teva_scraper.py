8111b8d95e75fd1497f1d2bf8fc36f8beee7543c
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2026-07-21 22:13:23 +0300

    fixed teva scraper

--- sites/teva_scraper.py
+++ sites/teva_scraper.py
@@ -1,30 +1,57 @@
+import html as html_mod
+import json
 from src.scrapers import Scraper
+from src.validate_city import validate_city
 
 
 class Teva(Scraper):
 
     def get_jobs(self):
-
         try:
             response = self.get_soup(verify=False)
         except Exception:
             return self.jobs_list
 
-        jobs = response.find_all('tr', class_='data-row')
+        positions = self._extract_positions(str(response))
 
-        for job in jobs:
-            link = 'https://careers.teva' + job.find('a', class_='jobTitle-link')['href']
-            title = job.find('a', class_='jobTitle-link').text
-            city = job.find('span', class_='jobLocation').text.split(',')[0].strip()
+        for job in positions:
+            city_raw = job.get('location', '').split(',')[0].strip()
+            city = validate_city(city_raw) if city_raw else 'Bucuresti'
+            title = job.get('name', '').strip()
+            link = job.get('canonicalPositionUrl', '')
 
-            self.get_jobs_dict(title, link, self.get_validated_city(city))
+            self.get_jobs_dict(title, link, city)
 
         return self.jobs_list
+
+    @staticmethod
+    def _extract_positions(raw_html):
+        decoded = html_mod.unescape(raw_html)
+        idx = decoded.find('"positions"')
+        if idx < 0:
+            return []
+
+        arr_start = decoded.find('[', idx)
+        if arr_start < 0:
+            return []
+
+        depth = 0
+        for i in range(arr_start, len(decoded)):
+            if decoded[i] == '[':
+                depth += 1
+            elif decoded[i] == ']':
+                depth -= 1
+                if depth == 0:
+                    try:
+                        return json.loads(decoded[arr_start:i + 1])
+                    except json.JSONDecodeError:
+                        return []
+        return []
 
 
 teva = Teva(
     company_name='teva',
-    url='https://careers.teva/search/?searchby=location&createNewAlert=false&q=&locationsearch=&geolocation=&optionsFacetsDD_facility=&optionsFacetsDD_department=Romania',
+    url='https://www.careers.teva/careers?domain=tevapharm.com&location=Romania&sort_by=relevance',
     logo_url='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Teva_Pharmaceuticals_logo.png/800px-Teva_Pharmaceuticals_logo.png'
 )
 teva.get_jobs()
